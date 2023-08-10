31b886d68b5b25aa46b925cb164bd4cdb9ebcc5e
unknown <f20201429@goa.bits-pilani.ac.in>
2023-08-10 10:50:47 +0530

    1st commit

---
+++ config.js
@@ -0,0 +1,14 @@
+// Polygon Mainnet DAI Contract Address
+const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
+// Random user's address that happens to have a lot of DAI on Polygon Mainnet
+// By the time you're readin' this, if this address doesn't have DAI on Polygon Mainnet,
+// switch it out for someone else who does have a lot of DAI
+const DAI_WHALE = "0x4aac95ebe2ea6038982566741d1860556e265f8b";
+
+// Mainnet Pool contract address
+const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
+module.exports = {
+  DAI,
+  DAI_WHALE,
+  POOL_ADDRESS_PROVIDER,
+};
